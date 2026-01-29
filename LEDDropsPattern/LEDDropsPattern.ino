3b4b590587b2d66471ba31d235e320b63bfca3d2
Liam D'Arcy <ldarcy3d@gmail.com>
2026-01-29 18:51:19 +1100

    fixed some flickering issues

--- LEDDropsPattern/LEDDropsPattern.ino
+++ LEDDropsPattern/LEDDropsPattern.ino
@@ -7,8 +7,8 @@
 #define FALLOFF 1
 #define LERP_SPEED 0.05
 #define BRIGHTNESS 255
-#define NEIGHBOUR_LERP_THRESHOLD 15
-#define NEIGHBOUR_LERP_SPEED 0.1
+#define NEIGHBOUR_LERP_THRESHOLD 30
+#define NEIGHBOUR_LERP_SPEED 0.2
 #define BASE_COLOUR 0xf77c3e
 
 #define SPAWN_DELAY_MIN 500
@@ -17,9 +17,12 @@
 #define DIM_DELAY_MAX 20
 #define CYCLE_DELAY_MIN 40
 #define CYCLE_DELAY_MAX 60
+#define LIFETIME_DELAY_MIN 500
+#define LIFETIME_DELAY_MAX 700
 
 CRGB leds[NUM_LEDS];
 CRGB vleds[NUM_LEDS]; // target virtual LED board which the real LEDs interpolate towards
+long vled_lifetime[NUM_LEDS];
 
 unsigned long prev_spawn_mills = 0;
 unsigned long prev_dim_mills = 0;
@@ -60,13 +63,12 @@ void loop() {
     }
   }
 
-  // dim both real LEDs and virtual LEDs at a variable rate
+  // dim LEDs
   if (curr_mills - prev_dim_mills >= dim_delay) {
     dim_delay = random(DIM_DELAY_MIN, DIM_DELAY_MAX);
     prev_dim_mills = curr_mills;
 
     for (int i = 0; i < NUM_LEDS; i++) {
-      dim_led(&vleds[i], 1);
       dim_led(&leds[i], 1);
     }
   }
@@ -75,7 +77,18 @@ void loop() {
   if (curr_mills - prev_spawn_mills >= spawn_delay) {
     spawn_delay = random(SPAWN_DELAY_MIN, SPAWN_DELAY_MAX);
     prev_spawn_mills = curr_mills;
-    vleds[random(0, NUM_LEDS)] = BASE_COLOUR;
+
+    int vled_index = random(0, NUM_LEDS);
+
+    vleds[vled_index] = BASE_COLOUR;
+    vled_lifetime[vled_index] = curr_mills + random(LIFETIME_DELAY_MIN, LIFETIME_DELAY_MAX);
+  }
+
+  // turn LED off after some time
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (vleds[i] != 0 && curr_mills >= vled_lifetime[i]) {
+      vleds[i] = 0;
+    }
   }
 
   FastLED.show();
@@ -117,9 +130,9 @@ void update_led(struct influence arr[], int arr_size, int pos) {
   avg.b = b_avg / total_strength;
 
   // find linear interpolation between current colour and target colour
-  leds[pos].r += ceil((avg.r - leds[pos].r) * LERP_SPEED * total_strength);
-  leds[pos].g += ceil((avg.g - leds[pos].g) * LERP_SPEED * total_strength);
-  leds[pos].b += ceil((avg.b - leds[pos].b) * LERP_SPEED * total_strength);
+  leds[pos].r += floor((avg.r - leds[pos].r) * LERP_SPEED * total_strength);
+  leds[pos].g += floor((avg.g - leds[pos].g) * LERP_SPEED * total_strength);
+  leds[pos].b += floor((avg.b - leds[pos].b) * LERP_SPEED * total_strength);
 
   // find the strongest neighbour and lerp to it
   CRGB for_neighbour = 0;
@@ -150,9 +163,9 @@ void update_led(struct influence arr[], int arr_size, int pos) {
 
   // lerp to strongest neighbour if the difference between current and neighbour exceeds the neighbour lerp threshold
   if (strongest_val - curr_val > NEIGHBOUR_LERP_THRESHOLD) {
-    leds[pos].r += ceil((strongest_neighbour.r - leds[pos].r) * NEIGHBOUR_LERP_SPEED);
-    leds[pos].g += ceil((strongest_neighbour.g - leds[pos].g) * NEIGHBOUR_LERP_SPEED);
-    leds[pos].b += ceil((strongest_neighbour.b - leds[pos].b) * NEIGHBOUR_LERP_SPEED);
+    leds[pos].r += floor((strongest_neighbour.r - leds[pos].r) * NEIGHBOUR_LERP_SPEED);
+    leds[pos].g += floor((strongest_neighbour.g - leds[pos].g) * NEIGHBOUR_LERP_SPEED);
+    leds[pos].b += floor((strongest_neighbour.b - leds[pos].b) * NEIGHBOUR_LERP_SPEED);
   }
 
   return;
